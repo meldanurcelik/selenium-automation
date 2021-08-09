d87b71fc6fff8fc2d9d45cfc46039da39e2d28d9
melda_celik <melda_celik@hotmail.com>
2021-08-09 21:28:52 +0300

    Selenium ile web elementleri bulma

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -1,6 +1,8 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test1 {
@@ -10,5 +12,26 @@ public class Test1 {
 
         driver.get("https://demoqa.com/text-box");
         driver.manage().window().maximize();
+
+        //Selenium ile web elementleri bulma
+        WebElement fullName = driver.findElement(By.id("userName"));
+        fullName.click();
+        fullName.sendKeys("Melda");
+
+        WebElement email = driver.findElement(new By.ByCssSelector(".mr-sm-2[placeholder='name@example.com']"));
+        email.click();
+        email.sendKeys("melda_celik@hotmail.com");
+
+        WebElement currentAddress = driver.findElement(new By.ByCssSelector(".form-control[id='currentAddress']"));
+        currentAddress.click();
+        currentAddress.sendKeys("Samsun, Türkiye");
+
+        WebElement permanentAddress = driver.findElement(new By.ByCssSelector("textarea#permanentAddress"));
+        permanentAddress.click();
+        permanentAddress.sendKeys("Samsun, Türkiye");
+
+        WebElement submitButton = driver.findElement(new By.ByCssSelector("button#submit"));
+        submitButton.click();
+
     }
 }
