a0e61b01c8e7f92d744e38a3ab4751cdfa87b33b
melda_celik <melda_celik@hotmail.com>
2021-08-11 21:33:31 +0300

    Selenium ile kırık görselleri bulma

--- src/test/java/tests/Test7.java
+++ src/test/java/tests/Test7.java
@@ -4,7 +4,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
@@ -19,10 +21,26 @@ public class Test7 {
 
         //Kırık link bulma
         HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request =  new HttpGet("https://demoqa.com/");
+        HttpGet request = new HttpGet("https://demoqa.com/");
         HttpResponse response = client.execute(request);
         int statusCode = response.getStatusLine().getStatusCode();
         System.out.println(statusCode);
 
+        //Kırık görselleri bulma
+        WebElement brokenImage = driver.findElement(By.xpath("//div/img[2]"));
+        String brokenImageUrl = brokenImage.getAttribute("src");
+        System.out.println(brokenImageUrl);
+
+        HttpClient client2 = HttpClientBuilder.create().build();
+        HttpGet request2 = new HttpGet("https://demoqa.com/");
+        HttpResponse response2 = client2.execute(request2);
+        int statusCode2 = response2.getStatusLine().getStatusCode();
+        System.out.println(statusCode2);
+
+        if (statusCode2 == 200) {
+            System.out.println("Valid image!");
+        } else if (statusCode2 == 500) {
+            System.out.println("Broken image!");
+        }
     }
 }
